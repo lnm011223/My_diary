5efe8db63544ac3cce348657feccb243ce5011f5
lnm011223 <2969827073@qq.com>
2023-06-11 20:23:34 +0800

    增加个人设置页面欢迎语修改

--- app/src/main/java/com/lnm011223/my_diary/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/lnm011223/my_diary/ui/settings/SettingsFragment.kt
@@ -8,7 +8,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -53,14 +55,16 @@ class SettingsFragment : Fragment() {
     @SuppressLint("CommitPrefEdits", "SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val prefs = activity?.getSharedPreferences("data",Context.MODE_PRIVATE)
-        val isNotice = prefs?.getBoolean("isNotice",false)
+        val prefs = activity?.getSharedPreferences("data", Context.MODE_PRIVATE)
+        val isNotice = prefs?.getBoolean("isNotice", false)
+        val welcometext = prefs?.getString("welcometext","天天开心！")
         binding.isNoticeButton.isChecked = isNotice!!
-        when(isNotice){
+        binding.welcomeText.text = welcometext
+        when (isNotice) {
             true -> {
                 binding.timecard.isVisible = true
-                val hour = prefs.getInt("hour",22)
-                val min = prefs.getInt("minute",30)
+                val hour = prefs.getInt("hour", 22)
+                val min = prefs.getInt("minute", 30)
                 binding.noticeTimeText.text = "$hour:$min"
             }
             false -> {
@@ -71,17 +75,47 @@ class SettingsFragment : Fragment() {
             when (isChecked) {
                 true -> {
                     binding.timecard.isVisible = true
-                    val editor = activity?.getSharedPreferences("data",Context.MODE_PRIVATE)?.edit()
-                    editor?.putBoolean("isNotice",true)
+                    val editor =
+                        activity?.getSharedPreferences("data", Context.MODE_PRIVATE)?.edit()
+                    editor?.putBoolean("isNotice", true)
                     editor?.apply()
                 }
 
                 false -> {
                     binding.timecard.isVisible = false
-                    val editor = activity?.getSharedPreferences("data",Context.MODE_PRIVATE)?.edit()
-                    editor?.putBoolean("isNotice",false)
+                    val editor =
+                        activity?.getSharedPreferences("data", Context.MODE_PRIVATE)?.edit()
+                    editor?.putBoolean("isNotice", false)
                     editor?.apply()
                 }
+            }
+
+        }
+
+        binding.changeUsernameBtn.setOnClickListener {
+            // 设置布局
+            val view = layoutInflater.inflate(R.layout.username_dialog_layout, null)
+            val input = view.findViewById<EditText>(R.id.input_text)
+
+            AlertDialog.Builder(requireActivity()).apply {
+
+                setTitle("请输入欢迎语：")
+
+                setView(view)
+                setPositiveButton("确认") { dialog, which ->
+                    val text = input.text.toString()
+                    // 处理文本输入
+                    binding.welcomeText.text = text
+                    val editor =
+                        activity?.getSharedPreferences("data", Context.MODE_PRIVATE)?.edit()
+                    editor?.putString("welcometext", text)
+                    editor?.apply()
+                }
+                setNegativeButton("取消") { dialog, which ->
+                    dialog.cancel()
+                }
+                create()
+                show()
             }
 
         }
@@ -92,7 +126,7 @@ class SettingsFragment : Fragment() {
                 .setTitle("请选择日期：")
                 .showBackNow(false)
                 .setBackGroundModel(R.drawable.shape_sheet_dialog_bg)
-                .setDisplayType(DateTimeConfig.HOUR,DateTimeConfig.MIN)
+                .setDisplayType(DateTimeConfig.HOUR, DateTimeConfig.MIN)
                 .showFocusDateInfo(false)
                 .setPickerLayout(R.layout.layout_time_picker_segmentation)
                 .setThemeColor(Color.parseColor("#3EB06A"))
@@ -106,9 +140,10 @@ class SettingsFragment : Fragment() {
                     val dateString = BaseUtil.second2Date(millisecond)
                     val hour = dateString.substring(8..9).toInt()
                     val min = dateString.substring(10..11).toInt()
-                    val editor = activity?.getSharedPreferences("data",Context.MODE_PRIVATE)?.edit()
-                    editor?.putInt("hour",hour)
-                    editor?.putInt("minute",min)
+                    val editor =
+                        activity?.getSharedPreferences("data", Context.MODE_PRIVATE)?.edit()
+                    editor?.putInt("hour", hour)
+                    editor?.putInt("minute", min)
                     editor?.apply()
                     binding.noticeTimeText.text = "$hour:$min"
 
